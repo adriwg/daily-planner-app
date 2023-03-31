a96983ef51ec51323b33f13b8cf155be0d35f694
AdrianWong <adrianwg04@gmail.com>
2023-03-31 21:56:45 +0100

    Modified the index.js, make the code more clean, added logic to show specific background color of different time input textarea.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var currentDay = $("#currentDay");
+var currentHr = Number(moment().format("H"));
 var events = [];
 
 $(document).ready(function () {
@@ -12,6 +13,7 @@ $(document).ready(function () {
         events = JSON.parse(localStorage.getItem("events"));
     }
     display_events();
+
 });
 
 
@@ -30,19 +32,48 @@ function init_events() {
             event: ""
         });
     }
-    console.log(events);
 }
 
 function display_events() {
     var html="";
     for (var i = 0; i < events.length; i++){
+        var time = Number(events[i].time);
         html += '<div class="row" data-hr="'+events[i].time+'" id='+events[i].time+'">';
-        html += '<div class="hour">'+events[i].time+'AM</div>';
-        html += '<textarea>'+events[i].event+'</textarea>';
+        html += '<div class="hour">'+to12Hrs(time)+'</div>';
+        html += '<textarea class='+eventClass(time)+'>'+events[i].event+'</textarea>';
         html += '<div class="saveBtn">';
         html += '<i class="fa-solid fa-floppy-disk fa-lg"></i>';
         html += '</div>';
         html += '</div>';
     }
     $(".time-block").append(html);
+}
+
+// Time format change from 24-hr to 12-hr
+function to12Hrs(t) {
+    var timeslot = "";
+    if(t<12){
+        timeslot = t+"AM";
+    }else{
+        if(t=="12"){
+            timeslot = t+"PM";
+        }else{
+            timeslot = t -12;
+            timeslot = timeslot.toString()+"PM";
+        }   
+    }
+    return timeslot;
+}
+
+// Get class name for the event textarea
+function eventClass(t){
+    var className="";
+    if(t < currentHr){
+        className = "past";
+    }else if(t == currentHr){
+        className = "present";
+    }else if(t > currentHr){
+        className = "future";
+    }
+    return className;
 }
