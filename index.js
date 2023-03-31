38d2816b444c7eddc3924d1667353d9c33c4853c
AdrianWong <adrianwg04@gmail.com>
2023-03-31 18:41:31 +0100

    Modified index.html and style.css, added display events logic in index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ $(document).ready(function () {
     
     getDate();
 
+    // Get events from local storage
+    if(localStorage.getItem("events") == null) {
+        init_events();    
+    }else{
+        events = JSON.parse(localStorage.getItem("events"));
+    }
+    display_events();
 });
 
 
@@ -12,5 +19,30 @@ $(document).ready(function () {
 function getDate() {
     var today = moment().format("dddd, MMMM Do");
     currentDay.text(today);
+}
 
+
+//Initial events
+function init_events() {
+    for (var i = 0; i < 9; i++) {
+        events.push({
+            time: (9+i).toString(),
+            event: ""
+        });
+    }
+    console.log(events);
+}
+
+function display_events() {
+    var html="";
+    for (var i = 0; i < events.length; i++){
+        html += '<div class="row" data-hr="'+events[i].time+'" id='+events[i].time+'">';
+        html += '<div class="hour">'+events[i].time+'AM</div>';
+        html += '<textarea>'+events[i].event+'</textarea>';
+        html += '<div class="saveBtn">';
+        html += '<i class="fa-solid fa-floppy-disk fa-lg"></i>';
+        html += '</div>';
+        html += '</div>';
+    }
+    $(".time-block").append(html);
 }
